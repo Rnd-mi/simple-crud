c71f422140317981be3ae3d97635d0ebeab86873
Ivan Stepanov <SireOfAll79@yandex.ru>
2023-07-30 20:18:19 +0300

    initial commit

---
+++ src/test/java/ru/stepanov/democrud/DemoCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.stepanov.democrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoCrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
