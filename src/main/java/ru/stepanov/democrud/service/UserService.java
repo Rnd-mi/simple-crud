6fc91a502cc904222064c05021ee2bdd99b817fe
Ivan Stepanov <SireOfAll79@yandex.ru>
2023-07-30 20:32:14 +0300

    feature: add user service with crud operations

---
+++ src/main/java/ru/stepanov/democrud/service/UserService.java
@@ -0,0 +1,32 @@
+package ru.stepanov.democrud.service;
+
+import org.springframework.stereotype.Service;
+import ru.stepanov.democrud.model.User;
+import ru.stepanov.democrud.repository.UserRepository;
+
+import java.util.Collection;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public Collection<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    public User findById(Long id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+    public void deleteById(Long id) {
+        userRepository.deleteById(id);
+    }
+}
