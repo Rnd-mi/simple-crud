c4127e41344e38b78f393c85058c1d409f422c68
Ivan Stepanov <SireOfAll79@yandex.ru>
2023-07-31 11:55:42 +0300

    feature: add all operations to controller

--- src/main/java/ru/stepanov/democrud/controller/UserController.java
+++ src/main/java/ru/stepanov/democrud/controller/UserController.java
@@ -3,6 +3,7 @@ package ru.stepanov.democrud.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import ru.stepanov.democrud.model.User;
@@ -28,19 +29,32 @@ public class UserController {
     }
 
     @GetMapping("/create")
-    public String createUserForm() {
+    public String createUserForm(User user) {
         return "user-create";
     }
 
-    @PostMapping
+    @PostMapping("/create")
     public String createUser(User user) {
         userService.saveUser(user);
         return "redirect:/users";
     }
 
+    @GetMapping("/delete/{id}")
+    public String deleteUser(@PathVariable Long id) {
+        userService.deleteById(id);
+        return "redirect:/users";
+    }
 
+    @GetMapping("/update/{id}")
+    public String updateUserForm(@PathVariable Long id, Model model) {
+        User user = userService.findById(id);
+        model.addAttribute("user", user);
+        return "user-update";
+    }
 
-
-
-
+    @PostMapping("/update")
+    public String updateUser(User user) {
+        userService.saveUser(user);
+        return "redirect:/users";
+    }
 }
